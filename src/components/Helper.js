e0abebb9381c77daefa7306f15bbda39a0a00f4c
anagoarmandine <anagoarmandine@gmail.com>
2022-09-22 20:29:47 +0100

    register

--- src/components/Helper.js
+++ src/components/Helper.js
@@ -1,5 +1,4 @@
 var CryptoJS = require("crypto-js");
-console.log(CryptoJS)
 export function decrypt(string) {
     return JSON.parse(CryptoJS.AES.decrypt(string, 'esign-DINE-07').toString(CryptoJS.enc.Utf8));
 }
