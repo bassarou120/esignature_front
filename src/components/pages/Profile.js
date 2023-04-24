f405a99c2e619700f6e49dd9de0e11566918763e
anagoarmandine <anagoarmandine@gmail.com>
2023-04-24 10:30:26 +0100

    profile page setting up

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -23,14 +23,14 @@ const Profile = ( ) => {
                         <div className="container-xxl flex-grow-1 container-p-y">
 
                             <h4 className="fw-bold py-3 mb-4">
-                                <span className="text-muted fw-light">Account Settings /</span> Account
+                                <span className="text-muted fw-light">Paramètre du compte /</span> Compte
                             </h4>
 
                             <div className="row">
                                 <div className="col-md-12">
                                     <ProfileUlHeader/>
                                     <div className="card mb-4">
-                                        <h5 className="card-header">Profile Details</h5>
+                                        <h5 className="card-header">Détail du profile</h5>
 
                                         <div className="card-body">
                                             <div className="d-flex align-items-start align-items-sm-center gap-4">
@@ -38,7 +38,7 @@ const Profile = ( ) => {
                                                      height="100" width="100" id="uploadedAvatar"/>
                                                 <div className="button-wrapper">
                                                     <label htmlFor="upload" className="btn btn-primary me-2 mb-4" tabIndex="0">
-                                                        <span className="d-none d-sm-block">Upload new photo</span>
+                                                        <span className="d-none d-sm-block">Ajouter/Changer photo de profile</span>
                                                         <i className="bx bx-upload d-block d-sm-none"/>
                                                         <input type="file" id="upload" className="account-file-input" hidden=""
                                                                accept="image/png, image/jpeg"/>
@@ -46,10 +46,10 @@ const Profile = ( ) => {
                                                     <button type="button"
                                                             className="btn btn-outline-secondary account-image-reset mb-4">
                                                         <i className="bx bx-reset d-block d-sm-none"/>
-                                                        <span className="d-none d-sm-block">Reset</span>
+                                                        <span className="d-none d-sm-block">Vider</span>
                                                     </button>
 
-                                                    <p className="text-muted mb-0">Allowed JPG, GIF or PNG. Max size of 800K</p>
+                                                    <p className="text-muted mb-0">Image JPG, GIF or PNG. Taille maximal de 800K</p>
                                                 </div>
                                             </div>
                                         </div>
@@ -58,27 +58,17 @@ const Profile = ( ) => {
                                             <form id="formAccountSettings" method="POST" onSubmit="return false">
                                                 <div className="row">
                                                     <div className="mb-3 col-md-6">
-                                                        <label htmlFor="firstName" className="form-label">First Name</label>
+                                                        <label htmlFor="firstName" className="form-label">Nom et prénom</label>
                                                         <input className="form-control" type="text" id="firstName" name="firstName"
                                                                value="John" autoFocus=""/>
                                                     </div>
                                                     <div className="mb-3 col-md-6">
-                                                        <label htmlFor="lastName" className="form-label">Last Name</label>
-                                                        <input className="form-control" type="text" name="lastName" id="lastName"
-                                                               value="Doe"/>
-                                                    </div>
-                                                    <div className="mb-3 col-md-6">
-                                                        <label htmlFor="email" className="form-label">E-mail</label>
-                                                        <input className="form-control" type="text" id="email" name="email"
-                                                               value="john.doe@example.com" placeholder="john.doe@example.com"/>
-                                                    </div>
-                                                    <div className="mb-3 col-md-6">
-                                                        <label htmlFor="organization" className="form-label">Organization</label>
+                                                        <label htmlFor="organization" className="form-label">Entreprise</label>
                                                         <input type="text" className="form-control" id="organization"
                                                                name="organization" value="ThemeSelection"/>
                                                     </div>
                                                     <div className="mb-3 col-md-6">
-                                                        <label className="form-label" htmlFor="phoneNumber">Phone Number</label>
+                                                        <label className="form-label" htmlFor="phoneNumber">Numero de téléphone</label>
                                                         <div className="input-group input-group-merge">
                                                             <span className="input-group-text">US (+1)</span>
                                                             <input type="text" id="phoneNumber" name="phoneNumber"
@@ -89,31 +79,28 @@ const Profile = ( ) => {
 
                                                 </div>
                                                 <div className="mt-2">
-                                                    <button type="submit" className="btn btn-primary me-2">Save changes</button>
-                                                    <button type="reset" className="btn btn-outline-secondary">Cancel</button>
+                                                    <button type="submit" className="btn btn-primary me-2">Enregistrer</button>
+                                                    <button type="reset" className="btn btn-outline-secondary">Annuler</button>
                                                 </div>
                                             </form>
                                         </div>
                                     </div>
                                     <div className="card">
-                                        <h5 className="card-header">Delete Account</h5>
+                                        <h5 className="card-header">Supprimer mon compte</h5>
                                         <div className="card-body">
                                             <div className="mb-3 col-12 mb-0">
                                                 <div className="alert alert-warning">
-                                                    <h6 className="alert-heading fw-bold mb-1">Are you sure you want to delete your
-                                                        account?</h6>
-                                                    <p className="mb-0">Once you delete your account, there is no going back. Please be
-                                                        certain.</p>
+                                                    <h6 className="alert-heading fw-bold mb-1">Êtes vous sûr de vouloir supprimer votre compte ?</h6>
+                                                    <p className="mb-0">Vous ne pouriez pas revenir en arrière.</p>
                                                 </div>
                                             </div>
                                             <form id="formAccountDeactivation" onSubmit="return false">
                                                 <div className="form-check mb-3">
                                                     <input className="form-check-input" type="checkbox" name="accountActivation"
                                                            id="accountActivation"/>
-                                                    <label className="form-check-label" htmlFor="accountActivation">I confirm my
-                                                        account deactivation</label>
+                                                    <label className="form-check-label" htmlFor="accountActivation">Je confirme la suppression</label>
                                                 </div>
-                                                <button type="submit" className="btn btn-danger deactivate-account">Deactivate Account
+                                                <button type="submit" className="btn btn-danger deactivate-account">Supprimer
                                                 </button>
                                             </form>
                                         </div>
